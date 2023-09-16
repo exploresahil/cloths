0e0f8279cbe8f4ab43a7f2103ae9c641cb1c5d69
Parag koche <paragkoche0@gmail.com>
2023-09-16 18:24:24 +0530

    ORDERS

---
+++ backend/Order.ts
@@ -0,0 +1,27 @@
+import SuperBase from "@/backend/Backend.client";
+
+export const makeOrder = async (product: JSON, user: any): Promise<any> => {
+  return await SuperBase.from("Order")
+    .insert({
+      product: product,
+      user: user.id,
+      cancel: false,
+    })
+    .select();
+};
+export const CancelOrder = async (order_id: string) => {
+  return await SuperBase.from("Order")
+    .update({
+      cancel: true,
+    })
+    .eq("id", order_id);
+};
+export const UpdateOrder = async (order_id: string, updateObject: any) => {
+  return await SuperBase.from("Order").update(updateObject).eq("id", order_id);
+};
+export const getOrders = (user_id: string) => {
+  return SuperBase.from("Order").select("*").eq("user", user_id);
+};
+export const getOrdersId = (order_id: string) => {
+  return SuperBase.from("Order").select("*").eq("id", order_id);
+};
