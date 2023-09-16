3848b72534ffcdde16c619d758773186cb45b4aa
Parag koche <paragkoche0@gmail.com>
2023-09-16 18:14:08 +0530

    cart backend

---
+++ backend/Cart.ts
@@ -0,0 +1,19 @@
+import SuperBase from "@/backend/Backend.client";
+
+export const AddCartOrder = (
+  product: JSON,
+  user_id: string,
+  how_many: number
+) => {
+  return SuperBase.from("Cart").insert({
+    how_many,
+    product: product,
+    user: user_id,
+  });
+};
+export const RemoveCartOrder = (id: string) => {
+  return SuperBase.from("Cart").delete().eq("id", id);
+};
+export const UpdateCartOrder = (id: string, data: any) => {
+  return SuperBase.from("Cart").update(data).eq("id", id).select("*");
+};
