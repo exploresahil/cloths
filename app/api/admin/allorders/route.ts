be8529cb12ecc29a81c47d7fa453c7be21368091
Parag koche <paragkoche0@gmail.com>
2023-09-26 19:47:59 +0530

    update

--- app/api/admin/allorders/route.ts
+++ app/api/admin/allorders/route.ts
@@ -1,4 +1,6 @@
 import db from "@/backend/Backend.client";
 export async function GET(request: Request) {
-  return new Response(JSON.stringify(await db.from("Order").select("*")));
+  return new Response(
+    JSON.stringify(await db.from("Order").select("*,user(*)"))
+  );
 }
