95bff96e740bb18ffcbe3b715b5bd858835e486f
Parag koche <paragkoche0@gmail.com>
2023-10-05 18:08:43 +0530

    error

--- app/api/paymentConfrom/[order_id]/route.ts
+++ app/api/paymentConfrom/[order_id]/route.ts
@@ -7,58 +7,59 @@ import { Order } from "@/types";
 import { client2 } from "@/backend/Backend.client";
 
 export async function POST(request: Request, { params }: any) {
-  const order_id = params.order_id;
-  const FormData = await request.formData();
-  await client2.from("ADMIN_LOGS").insert({
-    LEVEL: "INFO",
-    LOG: "PAYMENT-CONFORM-RUN ORDER_ID-" + order_id,
-  });
-
-  console.log();
-  await client2.from("ADMIN_LOGS").insert({
-    LEVEL: "INFO",
-    LOG: FormData.get("code")?.toString() + " ORDER_ID-" + order_id,
-  });
-  if (FormData.get("code") == "PAYMENT_SUCCESS") {
-    client2.from("ADMIN_LOGS").insert({
-      LEVEL: "INFO",
-      LOG: "PAYMENT SUCCESS ORDER_ID-" + order_id,
-    });
-    const data = await ConformPay(order_id);
+  try {
+    const order_id = params.order_id;
+    const FormData = await request.formData();
     await client2.from("ADMIN_LOGS").insert({
       LEVEL: "INFO",
-      LOG: `${JSON.stringify(data.data)}` + "data return into ConformPay",
-    });
-    //console.log("ordered --->", data.data.product[0].product.order_id);
-
-    const client = createClient({
-      ...clientConfig,
-      token: process.env.NEXT_SANITY_TOKEN,
+      LOG: "PAYMENT-CONFORM-RUN ORDER_ID-" + order_id,
     });
 
-    const orders = await data.data.product.map(
-      async (product: {
-        id: number;
-        product: products;
-        how_many: number;
-        created_at: string;
-      }) => {
-        console.log(product);
-        return await client
-          .patch(product.product._id)
-          .set({ isAvailable: false })
-          .commit();
-      }
-    );
+    console.log();
     await client2.from("ADMIN_LOGS").insert({
       LEVEL: "INFO",
-      LOG: `${JSON.stringify(orders)}` + "order getting isAvailable : false",
+      LOG: FormData.get("code")?.toString() + " ORDER_ID-" + order_id,
     });
-    console.log(orders);
+    if (FormData.get("code") == "PAYMENT_SUCCESS") {
+      client2.from("ADMIN_LOGS").insert({
+        LEVEL: "INFO",
+        LOG: "PAYMENT SUCCESS ORDER_ID-" + order_id,
+      });
+      const data = await ConformPay(order_id);
+      await client2.from("ADMIN_LOGS").insert({
+        LEVEL: "INFO",
+        LOG: `${JSON.stringify(data.data)}` + "data return into ConformPay",
+      });
+      //console.log("ordered --->", data.data.product[0].product.order_id);
 
-    /* client.patch(data.data.product.product._id) */
+      const client = createClient({
+        ...clientConfig,
+        token: process.env.NEXT_SANITY_TOKEN,
+      });
 
-    const res = new Response(`
+      const orders = await data.data.product.map(
+        async (product: {
+          id: number;
+          product: products;
+          how_many: number;
+          created_at: string;
+        }) => {
+          console.log(product);
+          return await client
+            .patch(product.product._id)
+            .set({ isAvailable: false })
+            .commit();
+        }
+      );
+      await client2.from("ADMIN_LOGS").insert({
+        LEVEL: "INFO",
+        LOG: `${JSON.stringify(orders)}` + "order getting isAvailable : false",
+      });
+      console.log(orders);
+
+      /* client.patch(data.data.product.product._id) */
+
+      const res = new Response(`
     <html>  
     <body>
     <script>
@@ -68,9 +69,15 @@ export async function POST(request: Request, { params }: any) {
     </html>
     
     `);
-    res.headers.set("Content-Type", "text/html; charset=utf-8");
-    return res;
+      res.headers.set("Content-Type", "text/html; charset=utf-8");
+      return res;
+    }
+    // work On sanity
+    return new Response("ok");
+  } catch (e) {
+    await client2.from("ADMIN_LOGS").insert({
+      LEVEL: "Error",
+      LOG: JSON.stringify(e),
+    });
   }
-  // work On sanity
-  return new Response("ok");
 }
